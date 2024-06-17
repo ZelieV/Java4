104ad7987ac2d9fc23f21aefad3df446e61c0793
ZelieV <zelie.vdr@gmail.com>
2024-06-17 15:16:22 +0200

    first commit

---
+++ Movies.java
@@ -0,0 +1,16 @@
+
+
+class TabIndiana { 
+    public static void main(String[] args) {
+        String[] titles = {"Raiders of the Lost Ark", "Indiana Jones and the Temple of Doom", "Indiana Jones and the Last Crusade"};
+        String[] actors = {"Harrison Ford, Karen Allen, Paul Freeman", "Harrison Ford, Kate Capshaw, Ke Huy Quan", "Harrison Ford, Kate Capshaw, Ke Huy Quan"};
+
+        for (int i = 0; i < titles.length; i++) {
+            System.out.println("Title: " + titles[i]);
+            System.out.println("Actors: " + actors[i]);
+            System.out.println(); 
+        }
+    }
+}
+
+
